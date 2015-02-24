bb025ce0d0c209179ddfe5ce8b860234d50ebe26
ultrashining <ultrashining@1bcea1d2-8757-4345-8607-a885a283c2c9>
2015-02-24 02:36:03 +0000

    zhTW update

--- trunk/DBM-PvP/localization.tw.lua
+++ trunk/DBM-PvP/localization.tw.lua
@@ -18,7 +18,7 @@ L:SetOptionLocalization({
 	ColorByClass		= "在得分視窗中設置玩家名為職業顏色",
 	ShowInviteTimer		= "顯示戰場組隊計時器",
 	AutoSpirit			= "自動釋放靈魂",
-	HideBossEmoteFrame	= "隱藏團隊首領表情框架"
+	HideBossEmoteFrame	= "在戰場時隱藏團隊首領表情框架&要塞/公會提示"
 })
 
 L:SetMiscLocalization({
