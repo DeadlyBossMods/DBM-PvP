99033d774d9408faf4c47d08c13d5fa092e43794
Kris <cblair91@gmail.com>
2019-08-06 19:14:11 +0100

    Update Warsong

--- DBM-PvP/Battlegrounds/Warsong.lua
+++ DBM-PvP/Battlegrounds/Warsong.lua
@@ -1,37 +1,37 @@
-﻿-- Warsong mod v3.0
--- rewrite by Nitram and Tandanu
---
--- thanks to LeoLeal, DiabloHu and Са°ЧТВ
-
-
-local mod		= DBM:NewMod("z2106", "DBM-PvP", 2)
-local L			= mod:GetLocalizedStrings()
+﻿local mod	= DBM:NewMod("z2106", "DBM-PvP", 2)
+local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision("@file-date-integer@")
 mod:SetZone(DBM_DISABLE_ZONE_DETECTION)
 
-local bgzone = false
 mod:RegisterEvents(
 	"ZONE_CHANGED_NEW_AREA"
 )
 
---local startTimer		= mod:NewTimer(62, "TimerStart", "132349")
-local flagTimer			= mod:NewTimer(12, "TimerFlag", "132483")
-local vulnerableTimer	= mod:NewNextTimer(60, 46392)
-
 do
+	local C_CVar = C_CVar
+	local bgzone = false
+	local cachedShowCastbar, cachedShowFrames, cachedShowPets = GetCVarBool("showArenaEnemyCastbar"), GetCVarBool("showArenaEnemyFrames"), GetCVarBool("showArenaEnemyPets")
+
 	local function WSG_Initialize(self)
-		if DBM:GetCurrentArea() == 489 or DBM:GetCurrentArea() == 2106 then--Old, New
+		if DBM:GetCurrentArea() == 489 or DBM:GetCurrentArea() == 2106 then
 			bgzone = true
 			self:RegisterShortTermEvents(
 				"CHAT_MSG_BG_SYSTEM_ALLIANCE",
 				"CHAT_MSG_BG_SYSTEM_HORDE",
-				"CHAT_MSG_BG_SYSTEM_NEUTRAL"
+				"CHAT_MSG_BG_SYSTEM_NEUTRAL",
+				"START_TIMER"
 			)
-
+			-- Fix for flag carriers not showing up
+			C_CVar.SetCVar("showArenaEnemyCastbar", "1")
+			C_CVar.SetCVar("showArenaEnemyFrames", "1")
+			C_CVar.SetCVar("showArenaEnemyPets", "1")
 		elseif bgzone then
 			bgzone = false
 			self:UnregisterShortTermEvents()
+			C_CVar.SetCVar("showArenaEnemyCastbar", cachedShowCastbar)
+			C_CVar.SetCVar("showArenaEnemyFrames", cachedShowFrames)
+			C_CVar.SetCVar("showArenaEnemyPets", cachedShowPets)
 		end
 	end
 	mod.OnInitialize = WSG_Initialize
@@ -41,23 +41,46 @@ do
 	end
 end
 
-function mod:CHAT_MSG_BG_SYSTEM_NEUTRAL(msg)
-	if msg == L.Vulnerable1 or msg == L.Vulnerable2 or msg:find(L.Vulnerable1) or msg:find(L.Vulnerable2) then
-		vulnerableTimer:Start()
-	end
-end
-
 do
+	local flagTimer			= mod:NewTimer(12, "TimerFlag", "132349")
+	local vulnerableTimer	= mod:NewNextTimer(60, 46392)
+
 	local function updateflagcarrier(self, event, arg1)
 		if string.match(arg1, L.ExprFlagCaptured) then
 			flagTimer:Start()
 			vulnerableTimer:Cancel()
 		end
 	end
+
 	function mod:CHAT_MSG_BG_SYSTEM_ALLIANCE(...)
 		updateflagcarrier(self, "CHAT_MSG_BG_SYSTEM_ALLIANCE", ...)
 	end
+
 	function mod:CHAT_MSG_BG_SYSTEM_HORDE(...)
 		updateflagcarrier(self, "CHAT_MSG_BG_SYSTEM_HORDE", ...)
+	end
+
+	function mod:CHAT_MSG_BG_SYSTEM_NEUTRAL(msg)
+		if msg == L.Vulnerable1 or msg == L.Vulnerable2 or string.find(msg, L.Vulnerable1) or string.find(msg, L.Vulnerable2) then
+			vulnerableTimer:Start()
+		end
+	end
+end
+
+do
+	local tonumber = tonumber
+	local remainingTimer = mod:NewTimer(0, "TimerRemaining", 2457)
+
+	function mod:START_TIMER(_, timeSeconds)
+		mod:Schedule(timeSeconds + 1, function()
+			local info = GetIconAndTextWidgetVisualizationInfo(6)
+			if info and info.state == 1 then
+				local minutes, seconds = string.match(info.text, "(%d+):(%d+)")
+				if minutes and seconds then
+					remainingTimer:SetTimer(tonumber(seconds) + (tonumber(minutes) * 60) + 1)
+					remainingTimer:Start()
+				end
+			end
+		end, self)
 	end
 end
