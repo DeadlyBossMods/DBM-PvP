dba91ef4d5ac99dc9fa2fcecef8beca967bb65ae
Kris <cblair91@gmail.com>
2020-01-16 16:09:51 +0000

    Derp

--- DBM-PvP/Battlegrounds/Deepwind.lua
+++ DBM-PvP/Battlegrounds/Deepwind.lua
@@ -14,7 +14,7 @@ do
 	function mod:OnInitialize()
 		if DBM:GetCurrentArea() == 2245 then -- Previously zone 1105
 			DBM:GetModByName("PvPGeneral"):SubscribeAssault(
-				519, -- Previously 519, now 1576
+				1576, -- Previously 519, now 1576
 				{["Quarry"] = {17,19}, ["Farm"] = {32,34},  ["Market"] = {208,209}, ["Ruins"] = {213,214}, ["Shrine"] = {218, 219}},
 				{1e-300, 10 / 12, 10 / 9, 10 / 6, 10 / 3, 30}
 			)
