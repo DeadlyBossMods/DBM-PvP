8e1fc3cbe2b6f0be2765196a133e6df88486ff79
QartemisT <QartemisT@gmail.com>
2021-03-14 22:53:21 +0000

    General fixes (#87)
    - Changed zone change event to loading screen disabled (far superior support)
    - Fix some cases of unregistering in battlegrounds
    - Cleanup PvPGeneral a bit.

--- DBM-PvP/Battlegrounds/SilvershardMines.lua
+++ DBM-PvP/Battlegrounds/SilvershardMines.lua
@@ -6,7 +6,7 @@ local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision("@file-date-integer@")
 mod:SetZone(DBM_DISABLE_ZONE_DETECTION)
-mod:RegisterEvents("ZONE_CHANGED_NEW_AREA")
+mod:RegisterEvents("LOADING_SCREEN_DISABLED")
 
 do
 	local bgzone = false
@@ -28,11 +28,11 @@ do
 		end
 	end
 
-	function mod:ZONE_CHANGED_NEW_AREA()
+	function mod:LOADING_SCREEN_DISABLED()
 		self:Schedule(1, Init, self)
 	end
-	mod.PLAYER_ENTERING_WORLD	= mod.ZONE_CHANGED_NEW_AREA
-	mod.OnInitialize			= mod.ZONE_CHANGED_NEW_AREA
+	mod.PLAYER_ENTERING_WORLD	= mod.LOADING_SCREEN_DISABLED
+	mod.OnInitialize			= mod.LOADING_SCREEN_DISABLED
 end
 
 local carts = {}
