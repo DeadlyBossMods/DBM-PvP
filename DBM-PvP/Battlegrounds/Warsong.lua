6e3df30010bfe8eba5d4137d6b1d9969589ef023
QartemisT <QartemisT@gmail.com>
2021-04-04 06:25:38 +0100

    Debom files

--- DBM-PvP/Battlegrounds/Warsong.lua
+++ DBM-PvP/Battlegrounds/Warsong.lua
@@ -1,4 +1,4 @@
-﻿local mod = DBM:NewMod(WOW_PROJECT_ID == WOW_PROJECT_CLASSIC and "z489" or "z2106", "DBM-PvP")
+local mod = DBM:NewMod(WOW_PROJECT_ID == WOW_PROJECT_CLASSIC and "z489" or "z2106", "DBM-PvP")
 
 mod:SetRevision("@file-date-integer@")
 mod:SetZone(DBM_DISABLE_ZONE_DETECTION)
