98d1d8e0084bf37458f228445b053d8d2d468a68
Artemis <QartemisT@gmail.com>
2022-09-01 18:02:12 +0100

    Misc changes for Wrath
    Change some checks to `not isRetail` instead, as it handles all 3classics
    Added `D4WC` addon comm

--- DBM-PvP/PvPGeneral.lua
+++ DBM-PvP/PvPGeneral.lua
@@ -4,7 +4,8 @@ local L		= mod:GetLocalizedStrings()
 local DBM = DBM
 local GetPlayerFactionGroup = GetPlayerFactionGroup or UnitFactionGroup -- Classic Compat fix
 local isClassic = WOW_PROJECT_ID == (WOW_PROJECT_CLASSIC or 2)
-local isTBC = WOW_PROJECT_ID == (WOW_PROJECT_BURNING_CRUSADE_CLASSIC or 5)
+local isBCC = WOW_PROJECT_ID == (WOW_PROJECT_BURNING_CRUSADE_CLASSIC or 5)
+local isWrath = WOW_PROJECT_ID == 5 and wowTOC >= 30000
 
 local playerFaction = GetPlayerFactionGroup("player")
 
@@ -85,7 +86,7 @@ do
 		local _, instanceType = IsInInstance()
 		if instanceType == "pvp" or instanceType == "arena" then
 			if not bgzone then
-				SendAddonMessage(isTBC and "D4BC" or isClassic and "D4C" or "D4", "H", "INSTANCE_CHAT")
+				SendAddonMessage(isWrath and "D4WC" or isBCC and "D4BC" or isClassic and "D4C" or "D4", "H", "INSTANCE_CHAT")
 				self:Schedule(3, DBM.RequestTimers, DBM)
 				if self.Options.HideBossEmoteFrame then
 					DBM:HideBlizzardEvents(1, true)
@@ -193,7 +194,7 @@ do
 	local flagTimer			= mod:NewTimer(12, "TimerFlag", "132483") -- Interface\\icons\\inv_banner_02.blp
 	local startTimer		= mod:NewTimer(120, "TimerStart", playerFaction == "Alliance" and "132486" or "132485") -- Interface\\Icons\\INV_BannerPVP_02.blp || Interface\\Icons\\INV_BannerPVP_01.blp
 	local vulnerableTimer, timerShadow, timerDamp
-	if not isClassic and not isTBC then
+	if isRetail then
 		vulnerableTimer	= mod:NewNextTimer(60, 46392)
 		timerShadow		= mod:NewNextTimer(90, 34709)
 		timerDamp		= mod:NewCastTimer(300, 110310)
@@ -214,7 +215,7 @@ do
 			end
 		end
 		local _, instanceType = IsInInstance()
-		if not isClassic and not isTBC and instanceType == "arena" then
+		if isRetail and instanceType == "arena" then
 			self:Schedule(timeSeconds + 1, function()
 				timerShadow:Start()
 				timerDamp:Start()
@@ -235,7 +236,7 @@ do
 				flagTimer:SetColor({r=1, g=0, b=0})
 				flagTimer:UpdateIcon("132485") -- Interface\\Icons\\INV_BannerPVP_01.blp
 			end
-			if not isClassic and not isTBC then
+			if isRetail then
 				vulnerableTimer:Cancel()
 			end
 		end
@@ -276,7 +277,7 @@ do
 		[5] = {1e-300, 1, 1.5, 2, 3.5, 30} -- Arathi/Deepwind
 	}
 
-	if isClassic or isTBC then
+	if not isRetail then
 		-- 2014 values seem ok https://github.com/DeadlyBossMods/DBM-PvP/blob/843a882eae2276c2be0646287c37b114c51fcffb/DBM-PvP/Battlegrounds/Arathi.lua#L32-L39
 		resourcesPerSec[5] = {1e-300, 10/12, 10/9, 10/6, 10/3, 30}
 	end
@@ -362,7 +363,7 @@ do
 		-- retail av
 		[91]    = 243,
 		-- classic av
-		[1459]  = isTBC and 243 or 304,
+		[1459]  = (isBCC or isWrath) and 243 or 304,
 		-- korrak
 		[1537]  = 243
 	}
@@ -374,15 +375,15 @@ do
 	}
 	local icons = {
 		-- Graveyard
-		[(isClassic or isTBC) and 3 or 4]      = State.ALLY_CONTESTED,
-		[(isClassic or isTBC) and 14 or 15]    = State.ALLY_CONTROLLED,
-		[(isClassic or isTBC) and 13 or 14]    = State.HORDE_CONTESTED,
-		[(isClassic or isTBC) and 12 or 13]    = State.HORDE_CONTROLLED,
+		[not isRetail and 3 or 4]   = State.ALLY_CONTESTED,
+		[not isRetail and 14 or 15] = State.ALLY_CONTROLLED,
+		[not isRetail and 13 or 14] = State.HORDE_CONTESTED,
+		[not isRetail and 12 or 13] = State.HORDE_CONTROLLED,
 		-- Tower/Lighthouse
-		[(isClassic or isTBC) and 8 or 9]      = State.ALLY_CONTESTED,
-		[(isClassic or isTBC) and 10 or 11]    = State.ALLY_CONTROLLED,
-		[(isClassic or isTBC) and 11 or 12]    = State.HORDE_CONTESTED,
-		[(isClassic or isTBC) and 9 or 10]     = State.HORDE_CONTROLLED,
+		[not isRetail and 8 or 9]   = State.ALLY_CONTESTED,
+		[not isRetail and 10 or 11] = State.ALLY_CONTROLLED,
+		[not isRetail and 11 or 12] = State.HORDE_CONTESTED,
+		[not isRetail and 9 or 10]  = State.HORDE_CONTROLLED,
 		-- Mine/Quarry
 		[17]                        = State.ALLY_CONTESTED,
 		[18]                        = State.ALLY_CONTROLLED,
@@ -444,7 +445,7 @@ do
 		[219]                       = State.HORDE_CONTESTED,
 		[216]                       = State.HORDE_CONTROLLED
 	}
-	local capTimer = mod:NewTimer((isTBC or isClassic) and 64 or 60, "TimerCap", "136002") -- Interface\\icons\\spell_misc_hellifrepvphonorholdfavor.blp
+	local capTimer = mod:NewTimer(isRetail and 60 or 64, "TimerCap", "136002") -- Interface\\icons\\spell_misc_hellifrepvphonorholdfavor.blp
 
 	function mod:AREA_POIS_UPDATED(widget)
 		local allyBases, hordeBases = 0, 0
